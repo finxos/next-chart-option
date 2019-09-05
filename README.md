a3d8525b70f29c8401511ed4179379608ca714ea
afeiship <1290657123@qq.com>
2019-09-05 12:14:57 +0800

    add: intiail

---
+++ README.md
@@ -0,0 +1,12 @@
+# next-chart-option
+> Parse chart option for next.
+
+## installation
+```bash
+npm install -S afeiship/next-chart-option --registry=https://registry.npm.taobao.org
+```
+
+## usage
+```js
+//DOCS here!
+```
---
+++ dist/next-chart-option.js
@@ -0,0 +1,44 @@
+/*!
+ * name: next-chart-option
+ * url: https://github.com/afeiship/next-chart-option
+ * version: 1.0.0
+ * date: 2019-09-05T04:14:53.612Z
+ * license: MIT
+ */
+
+(function() {
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('next-js-core2');
+  var NxChartData2set = nx.ChartData2set || require('next-chart-data2set');
+
+  // private package:
+  var echartsAdapter = require('echarts-adapter').echartsAdapter;
+  var toEcharts = echartsAdapter.toEcharts;
+  var assembleData = echartsAdapter.assembleData;
+  var adaptInst = echartsAdapter.adaptInst;
+
+  nx.chartOption = function(inData) {
+    var charts = inData.chart.charts;
+    var chartStyle = JSON.parse(charts[0].style);
+    var chartOption = chartStyle.chartOption;
+    var dateRange = chartStyle.dateRange;
+    var autoUpdate = chartStyle.autoUpdate;
+    var isRaw = chartStyle.format === 'raw';
+    if (isRaw) return chartStyle;
+    var seriesInfoset = NxChartData2set.info(inData.infolist);
+    var dataset = NxChartData2set.data(inData.datalist);
+    var sids = chartOption.seriesList.reduce(function(filtered, item) {
+      item.show && filtered.push(item);
+      return filtered;
+    });
+    var dataTable = assembleData(dataset, sids, dateRange, autoUpdate, 'date', seriesInfoset);
+    var option = toEcharts(chartStyle, dataTable, seriesInfoset);
+    return adaptInst(option);
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.chartOption;
+  }
+})();
+
+//# sourceMappingURL=next-chart-option.js.map
